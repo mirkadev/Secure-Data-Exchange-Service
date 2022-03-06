13a2b6d9d73a06833eb1588c77848520e2ca29db
Vladimir Mironov <vladimir.mironov1@simbirsoft.com>
2022-03-06 19:35:27 +0300

    Add logging into delete data service

--- src/domain/services/data/delete/delete-data.service.js
+++ src/domain/services/data/delete/delete-data.service.js
@@ -1,11 +1,14 @@
 const { ValidationException, NotFoundException } = require('../../../exceptions');
 
 class DeleteDataService {
-  constructor(deleteDataPort, deleteMetadataPort, loadMetadataPort, saveDeletedDataLogPort) {
+  constructor(deleteDataPort, deleteMetadataPort, loadMetadataPort, saveDeletedDataLogPort, logger) {
     this._deleteDataPort = deleteDataPort;
     this._deleteMetadataPort = deleteMetadataPort;
     this._loadMetadataPort = loadMetadataPort;
     this._saveDeletedDataLogPort = saveDeletedDataLogPort;
+    this._logger = logger;
+
+    this._logger.setName('DELETE-DATA-SERVICE');
   }
 
   async delete(adminCode) {
@@ -19,9 +22,24 @@ class DeleteDataService {
       throw new NotFoundException('Cannot find data for this admin code');
     }
 
-    await this._deleteDataPort.delete(metadata);
-    await this._deleteMetadataPort.delete(metadata);
-    await this._saveDeletedDataLogPort.save(metadata);
+    await this._deleteDataPort
+      .delete(metadata)
+      .then(() => this._logger.info(`File ${metadata.filename} was deleted`))
+      .catch((e) =>
+        this._logger.error(`Cannot delete data ${e}, filename: ${metadata.filename}, id: ${metadata.id}`, e),
+      );
+    await this._deleteMetadataPort
+      .delete(metadata)
+      .then(() => this._logger.info(`Metadata ${metadata.filename} was deleted`))
+      .catch((e) =>
+        this._logger.error(`Cannot delete metadata ${e}, filename: ${metadata.filename}, id: ${metadata.id}`, e),
+      );
+    await this._saveDeletedDataLogPort
+      .save(metadata)
+      .then(() => this._logger.info(`File ${metadata.filename} was logged`))
+      .catch((e) =>
+        this._logger.error(`Cannot save deleted data log ${e}, filename: ${metadata.filename}, id: ${metadata.id}`, e),
+      );
 
     return true;
   }
