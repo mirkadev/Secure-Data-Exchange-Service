8fbf3dcac67ca7d3c773f568d7ca0576434d1300
Vladimir Mironov <vladimir.mironov1@simbirsoft.com>
2022-03-06 19:15:58 +0300

    Add delete all data service

---
+++ src/domain/services/data/delete-all/delete-all-data.service.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-restricted-syntax */
+const { randomUUID } = require('crypto');
+const { CLEAR_CODE_UUID } = require('../../../constants');
+
+class DeleteAllDataService {
+  constructor(
+    loadClearCodePort,
+    saveClearCodePort,
+    deleteDataPort,
+    loadAllMetadataGeneratorPort,
+    deleteMetadataPort,
+    saveDeletedDataLogPort,
+  ) {
+    this._loadClearCodePort = loadClearCodePort;
+    this._saveClearCodePort = saveClearCodePort;
+    this._deleteDataPort = deleteDataPort;
+    this._loadAllMetadataGeneratorPort = loadAllMetadataGeneratorPort;
+    this._deleteMetadataPort = deleteMetadataPort;
+    this._saveDeletedDataLogPort = saveDeletedDataLogPort;
+  }
+
+  async deleteAll(clearCode) {
+    let allowDelete = false;
+    const foundClearCode = await this._loadClearCodePort.find();
+
+    if (!foundClearCode) {
+      if (clearCode === CLEAR_CODE_UUID) {
+        allowDelete = true;
+      }
+
+      throw new Error('Clear code is wrong');
+    }
+
+    if (!allowDelete && clearCode !== foundClearCode) {
+      throw new Error('Clear code is wrong');
+    }
+
+    const allMetadataGenerator = this._loadAllMetadataGeneratorPort();
+
+    process.nextTick(async () => {
+      for await (const metadata of allMetadataGenerator) {
+        await this._deleteDataPort.save(metadata);
+        await this._deleteMetadataPort.save(metadata);
+        await this._saveDeletedDataLogPort.save(metadata);
+      }
+    });
+
+    const newClearCodeUUID = randomUUID();
+    await this._saveClearCodePort.save(newClearCodeUUID);
+
+    return newClearCodeUUID;
+  }
+}
+
+module.exports = { DeleteAllDataService };
