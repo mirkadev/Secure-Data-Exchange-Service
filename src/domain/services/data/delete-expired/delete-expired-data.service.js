c80c5afd3a2cebd1fc83d477d0a1d41312ade66a
Vladimir Mironov <vladimir.mironov1@simbirsoft.com>
2022-03-06 19:32:11 +0300

    Add logging into delete expired data service

--- src/domain/services/data/delete-expired/delete-expired-data.service.js
+++ src/domain/services/data/delete-expired/delete-expired-data.service.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-restricted-syntax */
 class DeleteExpiredDataService {
-  constructor(loadExpiredMetadataGeneratorPort, deleteDataPort, deleteMetadataPort, saveDeletedDataLogPort) {
+  constructor(loadExpiredMetadataGeneratorPort, deleteDataPort, deleteMetadataPort, saveDeletedDataLogPort, logger) {
     this._loadExpiredMetadataGeneratorPort = loadExpiredMetadataGeneratorPort;
     this._deleteDataPort = deleteDataPort;
     this._deleteMetadataPort = deleteMetadataPort;
     this._saveDeletedDataLogPort = saveDeletedDataLogPort;
+    this._logger = logger;
+
+    this._logger.setName('DELETE-EXPIRED-DATA-SERVICE');
   }
 
   async deleteExpired() {
@@ -12,9 +15,27 @@ class DeleteExpiredDataService {
 
     process.nextTick(async () => {
       for await (const metadata of expiredMetadataGenerator) {
-        await this._deleteDataPort.save(metadata);
-        await this._deleteMetadataPort.save(metadata);
-        await this._saveDeletedDataLogPort.save(metadata);
+        await this._deleteDataPort
+          .delete(metadata)
+          .then(() => this._logger.info(`File ${metadata.filename} was deleted`))
+          .catch((e) =>
+            this._logger.error(`Cannot delete data ${e}, filename: ${metadata.filename}, id: ${metadata.id}`, e),
+          );
+        await this._deleteMetadataPort
+          .delete(metadata)
+          .then(() => this._logger.info(`Metadata ${metadata.filename} was deleted`))
+          .catch((e) =>
+            this._logger.error(`Cannot delete metadata ${e}, filename: ${metadata.filename}, id: ${metadata.id}`, e),
+          );
+        await this._saveDeletedDataLogPort
+          .save(metadata)
+          .then(() => this._logger.info(`File ${metadata.filename} was logged`))
+          .catch((e) =>
+            this._logger.error(
+              `Cannot save deleted data log ${e}, filename: ${metadata.filename}, id: ${metadata.id}`,
+              e,
+            ),
+          );
       }
     });
 
