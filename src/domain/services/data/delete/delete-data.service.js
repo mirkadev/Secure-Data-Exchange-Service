df6c50fcd120f2c728411b5007acdd4c7dc5b6ac
Vladimir Mironov <vladimir.mironov1@simbirsoft.com>
2022-03-06 19:00:18 +0300

    Add delete data service

---
+++ src/domain/services/data/delete/delete-data.service.js
@@ -0,0 +1,30 @@
+const { ValidationException, NotFoundException } = require('../../../exceptions');
+
+class DeleteDataService {
+  constructor(deleteDataPort, deleteMetadataPort, loadMetadataPort, saveDeletedDataLogPort) {
+    this._deleteDataPort = deleteDataPort;
+    this._deleteMetadataPort = deleteMetadataPort;
+    this._loadMetadataPort = loadMetadataPort;
+    this._saveDeletedDataLogPort = saveDeletedDataLogPort;
+  }
+
+  async delete(adminCode) {
+    if (!adminCode) {
+      throw ValidationException('The adminCode should be provided');
+    }
+
+    const metadata = await this._loadMetadataPort.findByAdminCode(adminCode);
+
+    if (!metadata) {
+      throw new NotFoundException('Cannot find data for this admin code');
+    }
+
+    await this._deleteDataPort.save(metadata);
+    await this._deleteMetadataPort.save(metadata);
+    await this._saveDeletedDataLogPort.save(metadata);
+
+    return true;
+  }
+}
+
+module.exports = { DeleteDataService };
