6be3488599b6ed9d534cd561883a3c2ef6efa487
Vladimir Mironov <vladimir.mironov1@simbirsoft.com>
2022-03-08 20:59:57 +0300

    Add error handlers

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
+/* eslint-disable no-console */
 const { DataExchangeApp } = require('./infra/http/app');
 
 const app = new DataExchangeApp();
 app.run();
+
+process.on('uncaughtException', (data) => {
+  console.errror('Unexpected uncaughtException error!', data);
+});
+
+process.on('unhandledRejection', (data) => {
+  console.errror('Unexpected unhandledRejection error!', data);
+});
